2b685a241b09928286fa35a3284f29e52ccb5df7
hqmq <michael@riesd.com>
2015-08-24 16:17:56 -0600

    test that game state is broadcasted initially

--- lib/ref/tic_tac_toe.ex
+++ lib/ref/tic_tac_toe.ex
@@ -7,28 +7,34 @@ defmodule Ref.TicTacToe do
     game_atom = "tictactoe:#{game_name}" |> String.to_atom
     case Process.whereis(game_atom) do
       nil ->
-        GenServer.start(__MODULE__, user, name: game_atom)
+        {:ok, pid} = GenServer.start(__MODULE__, "tictactoe:#{game_name}", name: game_atom)
+        GenServer.call(pid, {:join, user})
       pid ->
         GenServer.call(pid, {:join, user})
     end
   end
 
   ## Callbacks
-  def init(%{token: token, name: name}) do
-    players = %{} |> Dict.put(token, %{name: name, role: "X"})
+  def init(game_id) do
     {:ok, %{
       board: [nil, nil, nil, nil, nil, nil, nil, nil, nil],
-      players: players,
+      game_id: game_id,
+      players: %{},
+      whose_turn: "X",
     }, @timeout}
   end
 
   def handle_call({:join, %{token: token, name: name}}, _from, %{players: players}=state) do
     case Enum.count(players) do
+      0 ->
+        players = Dict.put(players, token, %{name: name, role: "X"})
+        {:reply, {:ok, "X"}, Dict.put(state, :players, players), @timeout}
       1 ->
         players = Dict.put(players, token, %{name: name, role: "O"})
-        {:reply, {:ok, self()}, Dict.put(state, :players, players)}
+        broadcast_state(state)
+        {:reply, {:ok, "O"}, Dict.put(state, :players, players), @timeout}
       _else ->
-        {:reply, {:error, "game is full"}, state}
+        {:reply, {:error, "game is full"}, state, @timeout}
     end
   end
 
@@ -37,5 +43,10 @@ defmodule Ref.TicTacToe do
   end
   def handle_info(msg, state) do
     {:stop, "Received unexpected message #{inspect msg}", state}
+  end
+
+  ## Private Functions
+  def broadcast_state(%{board: board, game_id: gid, whose_turn: whose_turn}) do
+    Ref.Endpoint.broadcast! gid, "state", %{board: board, whose_turn: whose_turn}
   end
 end
--- test/tic_tac_toe_game_test.exs
+++ test/tic_tac_toe_game_test.exs
@@ -1,16 +1,24 @@
 defmodule TicTacToeGameTest do
   use ExUnit.Case
+  import Phoenix.ChannelTest
 
   alias Ref.TicTacToe
+  @endpoint Ref.Endpoint
 
   test "games are created by joining" do
-    assert {:ok, _pid} = TicTacToe.join_or_create_game("test", %{token: "t", name: "rob"})
+    assert {:ok, "X"} = TicTacToe.join_or_create_game("test", %{token: "t", name: "rob"})
   end
 
   test "only 2 people can join a game" do
-    assert {:ok, pid1} = TicTacToe.join_or_create_game("test_full", %{token: "t", name: "rob"})
-    assert {:ok, pid2} = TicTacToe.join_or_create_game("test_full", %{token: "t2", name: "bob"})
-    assert pid1 == pid2
+    assert {:ok, "X"} = TicTacToe.join_or_create_game("test_full", %{token: "t", name: "rob"})
+    assert {:ok, "O"} = TicTacToe.join_or_create_game("test_full", %{token: "t2", name: "bob"})
     assert {:error, "game is full"} == TicTacToe.join_or_create_game("test_full", %{token: "t3", name: "snob"})
+  end
+
+  test "the game state is broadcast when the game is ready" do
+    @endpoint.subscribe(self(), "tictactoe:broadtest")
+    assert {:ok, "X"} = TicTacToe.join_or_create_game("broadtest", %{token: "t1", name: "george"})
+    assert {:ok, "O"} = TicTacToe.join_or_create_game("broadtest", %{token: "t2", name: "bob"})
+    assert_broadcast "state", %{board: [nil,nil,nil,nil,nil,nil,nil,nil,nil], whose_turn: "X"}
   end
 end
