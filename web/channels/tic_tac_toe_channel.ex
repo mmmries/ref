8a14faf1496b9ee1c6b9259df02a9f90d488ca13
hqmq <michael@riesd.com>
2015-08-24 23:20:04 -0600

    get rid of extra debug

--- web/channels/tic_tac_toe_channel.ex
+++ web/channels/tic_tac_toe_channel.ex
@@ -2,13 +2,11 @@ defmodule Ref.TicTacToeChannel do
   use Phoenix.Channel
 
   def join("tictactoe:"<>game_id, %{"token" => token, "name" => name}, socket) do
-    IO.puts "someone joined #{game_id}"
     {:ok, %{board: [nil,nil,nil,nil,nil,nil,nil,nil,nil]}, socket}
   end
 
   def handle_in("move", %{"token" => token, "square" => square}, socket) do
     broadcast! socket, "state", %{board: ["X",nil,nil,nil,nil,nil,nil,nil,nil]}
-    IO.puts "someone is on the move: #{token} => #{square}"
     {:noreply, socket}
   end
 end
