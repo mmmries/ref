05a65c32696bec3961a8885f8565b91533903f26
hqmq <michael@riesd.com>
2015-08-26 18:23:52 -0500

    add a route, action and template for playing a tic-tac-toe game

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule Ref.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def play(conn, %{"id" => id}) do
+    render conn, "play.html", id: id
+  end
 end
