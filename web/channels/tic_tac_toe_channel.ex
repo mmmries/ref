769e912e8dc2f06572774622c85014d87fe20aa1
hqmq <michael@riesd.com>
2015-08-26 09:03:39 -0500

    fixing the channel based on experimentation, responding with errors was wrong

--- web/channels/tic_tac_toe_channel.ex
+++ web/channels/tic_tac_toe_channel.ex
@@ -1,12 +1,31 @@
 defmodule Ref.TicTacToeChannel do
   use Phoenix.Channel
 
-  def join("tictactoe:"<>game_id, %{"token" => token, "name" => name}, socket) do
-    {:ok, %{board: [nil,nil,nil,nil,nil,nil,nil,nil,nil]}, socket}
+  def join(topic, %{"token" => token, "name" => name}, socket) do
+    case Ref.TicTacToe.join_or_create_game(topic, %{token: token, name: name}) do
+      {:ok, role} -> {:ok, %{role: role}, socket}
+      {:ok, role, :broadcast, game_state} ->
+        send self(), {:initial_game_state, socket, game_state}
+        {:ok, %{role: role}, socket}
+      {:error, reason} ->
+        {:error, %{message: reason}}
+    end
   end
 
   def handle_in("move", %{"token" => token, "square" => square}, socket) do
-    broadcast! socket, "state", %{board: ["X",nil,nil,nil,nil,nil,nil,nil,nil]}
-    {:noreply, socket}
+    case TicTacToe.move(socket.topic, %{token: token, square: square}) do
+      {:ok, game_state} ->
+        broadcast! socket, "state", game_state
+        {:noreply, socket}
+      {:game_over, game_state, winner} ->
+        broadcast! socket, "game_over", %{game_state | winner: winner}
+        {:noreply, socket}
+      {:error, reason} ->
+        {:error, %{message: reason}}
+    end
+  end
+
+  def handle_info({:initial_game_state, socket, game_state}) do
+    broadcast! socket, "state", game_state
   end
 end
