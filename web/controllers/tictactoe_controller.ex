22731a6bdbff8f260a7ef3a8ea4df375027c99ed
hqmq <michael@riesd.com>
2015-08-28 18:29:08 -0500

    add a tic tac toe tutorial page

--- web/controllers/tictactoe_controller.ex
+++ web/controllers/tictactoe_controller.ex
@@ -5,6 +5,10 @@ defmodule Ref.TictactoeController do
     render conn, "play.html", id: id, just_watching: false
   end
 
+  def tutorial(conn, %{}) do
+    render conn, "tutorial.html"
+  end
+
   def watch(conn, %{"id" => id}) do
     render conn, "play.html", id: id, just_watching: true
   end
--- web/router.ex
+++ web/router.ex
@@ -9,20 +9,13 @@ defmodule Ref.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", Ref do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/tictactoe/tutorial", TictactoeController, :tutorial
     get "/tictactoe/play/:id", TictactoeController, :play
     get "/tictactoe/watch/:id", TictactoeController, :watch
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Ref do
-  #   pipe_through :api
-  # end
 end
