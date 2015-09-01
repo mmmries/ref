b2305b93aa4b73d0218308f77ba9c403c4023f6b
hqmq <michael@riesd.com>
2015-08-31 22:58:31 -0600

    display a list of ongoing games

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule Ref.PageController do
   use Ref.Web, :controller
+  alias Ref.TicTacToe
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", ongoing_games: ongoing_game_ids
+  end
+
+  defp ongoing_game_ids do
+    Ref.TicTacToe.ongoing_games |> Enum.map &( String.replace(&1, "tictactoe:", "") )
   end
 end
