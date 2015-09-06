c6f2a7a8561e6982148be7ce8c55f58324e97c9b
hqmq <michael@riesd.com>
2015-09-05 22:23:18 -0600

    a random ai opponent

---
+++ lib/ref/tictactoe/random.ex
@@ -0,0 +1,50 @@
+defmodule Ref.TicTacToe.Random do
+  use GenServer
+  @name "Random AI"
+
+  # Public API
+  def start(topic, sleep_between_moves \\ 0) do
+    GenServer.start(__MODULE__, {topic, sleep_between_moves})
+  end
+
+  # Callbacks
+  def init({topic, sleep_between_moves}) do
+    :rand.seed(:exs1024)
+    token = :rand.uniform |> Float.to_string
+    {:ok, role} = Ref.TicTacToe.join_or_create_game(topic, %{token: token, name: @name})
+    Ref.Endpoint.subscribe(self, topic)
+    state = %{topic: topic, token: token, role: role, sleep_between_moves: sleep_between_moves}
+    IO.inspect state
+    {:ok, state}
+  end
+
+  def handle_info(%{event: "state", payload: %{board: board, whose_turn: role}}, %{role: role, sleep_between_moves: sleep_between_moves}=state) do
+    %{token: token, topic: topic} = state
+    idx = pick_index(board)
+    :timer.sleep(sleep_between_moves)
+    {:ok, _game_state} = Ref.TicTacToe.move(topic, %{token: token, square: idx})
+    {:noreply, state}
+  end
+
+  def handle_info(%{event: "game_over"}, state) do
+    {:stop, :normal, state}
+  end
+
+  def handle_info(msg, state) do
+    IO.puts "Random AI received a message"
+    IO.inspect msg
+    {:noreply, state}
+  end
+
+  # Private API
+  def get_playable_indices(board) do
+    Enum.with_index(board)
+      |> Enum.filter(fn({nil, _idx}) -> true
+                       (_) -> false end)
+      |> Enum.map fn({nil,idx}) -> idx end
+  end
+
+  def pick_index(board) do
+    get_playable_indices(board) |> Enum.shuffle |> List.first
+  end
+end
--- web/channels/tic_tac_toe_channel.ex
+++ web/channels/tic_tac_toe_channel.ex
@@ -1,11 +1,15 @@
 defmodule Ref.TicTacToeChannel do
   use Phoenix.Channel
 
-  def join(_topic, %{"just_watching" => true}, socket), do: {:ok, %{}, socket}
+  def join(_topic, %{"just_watching" => true}, socket) do
+    send self, :broadcast_game_state
+    {:ok, %{}, socket}
+  end
 
-  def join(topic, %{"token" => token, "name" => name}, socket) do
+  def join(topic, %{"token" => token, "name" => name}=join, socket) do
     case Ref.TicTacToe.join_or_create_game(topic, %{token: token, name: name}) do
       {:ok, role} ->
+        start_ai_if_requested(topic, join)
         send self, :broadcast_game_state
         {:ok, %{role: role}, socket}
       {:error, reason} ->
@@ -28,8 +32,17 @@ defmodule Ref.TicTacToeChannel do
   end
 
   def handle_info(:broadcast_game_state, socket) do
-    {:ok, game_state} = Ref.TicTacToe.current_state(socket.topic)
-    broadcast! socket, "state", game_state
+    try do
+      {:ok, game_state} = Ref.TicTacToe.current_state(socket.topic)
+      broadcast! socket, "state", game_state
+    catch
+      :exit, {:noproc, _} -> nil
+    end
     {:noreply, socket}
   end
+
+  defp start_ai_if_requested(topic, %{"ai" => "random"}) do
+    Ref.TicTacToe.Random.start(topic, 1_000)
+  end
+  defp start_ai_if_requested(_topic, _message), do: nil
 end
