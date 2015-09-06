4ee210654053201ac67b3a0c44fb583b2a38c915
hqmq <michael@riesd.com>
2015-09-06 14:56:36 -0600

    the channel does not need to broadcast state updates anymore

--- web/channels/tic_tac_toe_channel.ex
+++ web/channels/tic_tac_toe_channel.ex
@@ -19,12 +19,9 @@ defmodule Ref.TicTacToeChannel do
 
   def handle_in("move", %{"token" => token, "square" => square}, socket) do
     case Ref.TicTacToe.move(socket.topic, %{token: token, square: square}) do
-      {:ok, game_state} ->
-        broadcast! socket, "state", game_state
+      {:ok, _game_state} ->
         {:noreply, socket}
-      {:game_over, game_state, winner} ->
-        complete_state = Map.put(game_state, :winner, winner)
-        broadcast! socket, "game_over", complete_state
+      {:game_over, _game_state} ->
         {:noreply, socket}
       {:error, reason} ->
         {:reply, {:error, %{message: reason}}, socket}
