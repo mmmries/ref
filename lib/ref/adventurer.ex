323bc6dd30c4285146ae874ce431c3af07d2658b
Michael Ries <michael@riesd.com>
2015-11-28 11:43:43 -0700

    add ability for heros to move

--- lib/ref/adventurer.ex
+++ lib/ref/adventurer.ex
@@ -13,6 +13,10 @@ defmodule Ref.Adventurer do
     GenServer.call(pid, {:join, user})
   end
 
+  def move(game_id, name, movement) do
+    GenServer.call(game_id, {:move, name, movement})
+  end
+
   ## Callbacks
   def init(game_id) do
     {:ok, %{
@@ -21,15 +25,46 @@ defmodule Ref.Adventurer do
     }, @timeout}
   end
 
-  def handle_call({:join, %{name: name, role: "hero"}=player}, {pid, _ref}, %{players: players}=state) do
-    player = Map.put(player, :pid, pid)
+  def handle_call({:join, %{name: name}=player}, {pid, _ref}, %{players: players}=state) do
+    player = initialize_player(player, pid)
+    send_player_status(player, "Your adventure begins...")
     new_players = Map.put(players, name, player)
-    initialize_player(player)
     {:reply, :ok, %{state | players: new_players}, @timeout}
   end
 
+  def handle_call({:move, name, movement}, _from, %{players: players}=state) do
+    case attempt_move(players, name, movement) do
+      {:ok, new_player} ->
+        send_player_status(new_player, "You have moved")
+        {:reply, :ok, %{state | players: Map.put(state.players, name, new_player)}, @timeout}
+      {:error, reason} ->
+        {:reply, {:error, reason}, state, @timeout}
+    end
+  end
+
   ## Private Functions
-  def initialize_player(%{pid: pid}) do
-    send pid, {:status, %{position: {0,0}, health: 100.0, message: "ohai"}}
+  defp attempt_move(players, name, movement) do
+    case Map.get(players, name) do
+      nil -> {:error, "unknown user"}
+      player -> attempt_move(player, movement)
+    end
+  end
+  defp attempt_move(%{position: {x,y}}=player, {dx,dy}) do
+    if legal_move?(player, {dx, dy}) do
+      {:ok, %{player | position: {x+dx, y+dy}}}
+    else
+      {:error, "illegal move"}
+    end
+  end
+
+  defp initialize_player(%{name: name, role: "hero"}, pid) do
+    %{name: name, role: "hero", pid: pid, position: {0,0}, health: 100.0}
+  end
+
+  defp legal_move?(%{role: "hero"}, {dx, dy}) when dx > 1 or dy > 1 or dx < -1 or dy < -1, do: false
+  defp legal_move?(_player,_movement), do: true
+
+  defp send_player_status(%{pid: pid, health: h, position: p}, message) do
+    send pid, {:status, %{health: h, position: p, message: message}}
   end
 end
--- test/adventurer_test.exs
+++ test/adventurer_test.exs
@@ -11,4 +11,24 @@ defmodule AdventurerTest do
     assert is_integer(y)
     assert is_binary(message)
   end
+
+  test "heros can move" do
+    :ok = Adventurer.join(@game_id, %{name: "me", role: "hero"})
+    assert_receive {:status, %{position: {x, y}}}
+    expected_x = x + 1
+    assert :ok == Adventurer.move(@game_id, "me", {1,0})
+    assert_receive {:status, %{position: {^expected_x, ^y}}}
+  end
+
+  test "unknown users cannot move" do
+    :ok = Adventurer.join(@game_id, %{name: "me", role: "hero"})
+    assert_receive {:status, _status}
+    assert {:error, "unknown user"} = Adventurer.move(@game_id, "someone", {1,0})
+  end
+
+  test "heros cannot move too far" do
+    :ok = Adventurer.join(@game_id, %{name: "me", role: "hero"})
+    assert_receive {:status, _status}
+    assert {:error, "illegal move"} = Adventurer.move(@game_id, "me", {100, 0})
+  end
 end
