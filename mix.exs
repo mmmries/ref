510cbf3890f808f031d1691ae5870094821fd11d
Michael Ries <michael@riesd.com>
2017-11-04 15:35:40 -0600

    update to phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Ref.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
